b1dae1561cf28089af9eb297ec53ce92ce9d58cd
itsBuggingMe <lohj002@gmail.com>
2025-01-05 15:45:45 -0500

    added agg inlining

--- Frent/World.Create.cs
+++ Frent/World.Create.cs
@@ -13,6 +13,7 @@ namespace Frent;
 //it just so happens Archetype and Create both end with "e"
 partial class World
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public Entity Create<T>(in T comp)
     {
         Archetype archetype = WorldArchetypeTable[Archetype<T>.IDasUInt] ??= Archetype<T>.CreateNewArchetype(this);
@@ -24,6 +25,8 @@ partial class World
         //manually inlined from World.CreateEntityFromLocation
         //The jit likes to inline the outer create function and not inline
         //the inner functions - benchmarked to improve perf by 10-20%
+        //also i have no clue why, but according to my benchmarks, not putting any method
+        //impl attribute on this method is 2x as slow as putting either AggInlining, or NoInlining
 
         var (id, version) = _recycledEntityIds.TryPop(out var v) ? v : (_nextEntityID++, (ushort)0);
         EntityTable[(uint)id] = (eloc, version);
