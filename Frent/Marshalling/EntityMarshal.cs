342aa0d1d898e1b693333f1473a144fa3951815f
itsBuggingMe <lohj002@gmail.com>
2025-03-13 20:47:14 -0400

    bump

--- Frent/Marshalling/EntityMarshal.cs
+++ Frent/Marshalling/EntityMarshal.cs
@@ -3,15 +3,16 @@
 namespace Frent.Marshalling;
 
 /// <summary>
-/// Contains unsafe methods for higher preformance operations.
+/// Unsafe methods to write even faster code! Users are expected to know what they are doing and improper usage can result in corrupting world state and segfaults.
 /// </summary>
-/// <remarks>API consumers are expected to understand the internals. Improper unsage could result in world corruption, memory corruption, or segmentation faults.</remarks>
+/// <remarks>The APIs in this class are less stable, as many depend on implementation details.</remarks>
 public static class EntityMarshal
 {
     public static World? GetWorld(Entity entity)
     {
         return GlobalWorldTables.Worlds.UnsafeIndexNoResize(entity.EntityID);
     }
+
     public static int EntityID(Entity entity)
     {
         return entity.EntityID;
--- Frent/Marshalling/WorldMarshal.cs
+++ Frent/Marshalling/WorldMarshal.cs
@@ -3,6 +3,10 @@ using Frent.Updating.Runners;
 
 namespace Frent.Marshalling;
 
+/// <summary>
+/// Unsafe methods to write even faster code! Users are expected to know what they are doing and improper usage can result in corrupting world state and segfaults.
+/// </summary>
+/// <remarks>The APIs in this class are less stable, as many depend on implementation details.</remarks>
 public static class WorldMarshal
 {
     public static ref T GetComponent<T>(World world, Entity entity)
@@ -20,14 +24,14 @@ public static class WorldMarshal
     public static ref T Get<T>(World world, int entityID)
     {
 
-        EntityLookup location = world.EntityTable.UnsafeIndexNoResize(entityID);
+        EntityLocation location = world.EntityTable.UnsafeIndexNoResize(entityID);
 
-        Archetype archetype = location.Location.Archetype;
+        Archetype archetype = location.Archetype;
 
         int compIndex = archetype.GetComponentIndex<T>();
 
         //Components[0] null; trap
         ComponentStorage<T> storage = UnsafeExtensions.UnsafeCast<ComponentStorage<T>>(archetype.Components.UnsafeArrayIndex(compIndex));
-        return ref storage[location.Location.Index];
+        return ref storage[location.Index];
     }
 }
