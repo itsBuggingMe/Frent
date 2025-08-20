05c9f5a1020babfc2d92fc9d3c2321d37a3f4c90
itsBuggingMe <lohj002@gmail.com>
2025-08-20 08:48:04 -0400

    create fuzzing project

---
+++ Frent.Fuzzing/Program.cs
@@ -0,0 +1,23 @@
+﻿using Frent.Fuzzing.Runner;
+
+namespace Frent.Fuzzing;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        if(args.Length == 0)
+        {
+            
+        }
+        else
+        {
+            Assert.Fuzz(args);
+        }
+    }
+
+    private static void LaunchFuzzProcess(int seed, int count)
+    {
+        
+    }
+}
