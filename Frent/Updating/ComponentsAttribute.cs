641278a21f8e571e93ad5e8e5895f61eae70cbb4
itsBuggingMe <lohj002@gmail.com>
2025-10-13 20:23:35 -0400

    public api for including/excluding components and tags

---
+++ Frent/Updating/ComponentsAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace Frent.Updating;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class IncludesComponentsAttribute(params Type[] types) : Attribute
+{
+    public Type[] Includes { get; } = types;
+}
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class ExcludesComponentsAttribute(params Type[] types) : Attribute
+{
+    public Type[] Excludes { get; } = types;
+}
---
+++ Frent/Updating/TagsAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace Frent.Updating;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class IncludesTagsAttribute(params Type[] types) : Attribute
+{
+    public Type[] Includes { get; } = types;
+}
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class ExcludesTagsAttribute(params Type[] types) : Attribute
+{
+    public Type[] Excludes { get; } = types;
+}
