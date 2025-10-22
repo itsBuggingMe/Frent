f38302c88c0c5d06a4bfb3f613b572853327a3e1
Paperclip <lohj002@gmail.com>
2025-10-22 12:55:03 -0400

    Create ITag

---
+++ Frent/Components/ITag.cs
@@ -0,0 +1,7 @@
+namespace Frent.Components;
+
+/// <summary>
+/// Indicates a type is intended as a type type.
+/// </summary>
+/// <remarks>Types with this tag will be automatically registered for serialization.</remarks>
+public interface ITag;
