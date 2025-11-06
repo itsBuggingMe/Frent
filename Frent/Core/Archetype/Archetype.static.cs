eac69a9e5928d1c8af9e354a2e1cd848d6e56917
itsBuggingMe <lohj002@gmail.com>
2025-11-06 16:59:10 -0500

    compile time and runtime checks for duplicate type parameters

--- Frent/Core/Archetype/Archetype.static.cs
+++ Frent/Core/Archetype/Archetype.static.cs
@@ -180,6 +180,8 @@ partial class Archetype
             }
             else
             {
+                MemoryHelpers.AssertComponentIDsUnique(types);
+
                 int nextIDInt = ++NextArchetypeID;
                 if (nextIDInt == ushort.MaxValue)
                     throw new InvalidOperationException($"Exceeded maximum unique archetype count of 65535");
