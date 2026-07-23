5445b5c599ba0bb21bda0567a88137d648f9616a
itsBuggingMe <lohj002@gmail.com>
2026-07-23 12:07:02 -0400

    LinkID

---
+++ Frent/Core/LinkID.cs
@@ -0,0 +1,47 @@
+namespace Frent.Core;
+
+// Basically copied from TagID.cs
+
+/// <summary>
+/// Represents a specific type as a link, and can be used for link related queries
+/// </summary>
+public readonly struct LinkID : ITypeID, IEquatable<LinkID>
+{
+    internal LinkID(ushort id) => RawValue = id;
+    internal readonly ushort RawValue;
+
+    /// <summary>
+    /// The type that this LinkID represents
+    /// </summary>
+    public Type Type => Link.LinkTable[RawValue];
+
+    ushort ITypeID.Value => RawValue;
+
+    /// <summary>
+    /// Checks if this LinkID instance represents the same type as <paramref name="other"/>
+    /// </summary>
+    /// <param name="other">The link to compare against</param>
+    /// <returns><see langword="true"/> when they represent the same type, <see langword="false"/> otherwise</returns>
+    public readonly bool Equals(LinkID other) => other.RawValue == RawValue;
+    /// <summary>
+    /// Checks if this LinkID instance represents the same type as <paramref name="other"/>
+    /// </summary>
+    /// <param name="other">The link to compare against</param>
+    /// <returns><see langword="true"/> when they represent the same type, <see langword="false"/> otherwise</returns>
+    public override bool Equals(object? other) => other is LinkID t && RawValue == t.RawValue;
+    /// <summary>
+    /// Checks if two <see cref="LinkID"/>s represent the same type
+    /// </summary>
+    /// <returns><see langword="true"/> when they represent the same type, <see langword="false"/> otherwise</returns>
+    public static bool operator ==(LinkID left, LinkID right) => left.RawValue == right.RawValue;
+    /// <summary>
+    /// Checks if two <see cref="LinkID"/>s represent a different type
+    /// </summary>
+    /// <returns><see langword="false"/> when they represent the same type, <see langword="true"/> otherwise</returns>
+    public static bool operator !=(LinkID left, LinkID right) => left.RawValue != right.RawValue;
+    /// <summary>
+    /// Gets the hashcode of this <see cref="LinkID"/>
+    /// </summary>
+    /// <returns>A unique code representing the <see cref="LinkID"/></returns>
+    public override int GetHashCode() => RawValue;
+}
